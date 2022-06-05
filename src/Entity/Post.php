16ed90614e22d093aede187f6b73f014c3dc1eba
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-06-05 14:42:29 +0200

    normalization

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,34 +5,48 @@ namespace App\Entity;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 // Crée automatiquement tous les endpoints et la doc pour la classe Post
 // https://api-platform.com/docs/core/getting-started/#mapping-the-entities
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read:collection']],    // Permet de déterminer quel groupe de propriétés nous renvoyons dans la réponse: https://api-platform.com/docs/core/serialization/
+    itemOperations: [   // Définit les opérations et les groupes pour les endpoints concernant un item (/{id})
+        'get' => [
+            'normalization_context' => ['groups' => ['read:collection', 'read:get']] 
+        ]
+    ],
+    denormalizationContext: ['groups' => ['write']],    
+)]
 class Post
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read:collection'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collection'])]
     private $title;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collection'])]
     private $slug;
 
     #[ORM\Column(type: 'text')]
     private $content;
 
     #[ORM\Column(type: 'datetime')]
+    #[Groups(['read:get'])]
     private $createdAt;
 
     #[ORM\Column(type: 'datetime')]
     private $updatedAt;
 
     #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'posts')]
+    #[Groups(['read:get'])]
     private $category;
 
     public function getId(): ?int
